e68837c19052fd522cc1249f969dbc025c550cc3
Victoria Meng <nights1ststar@protonmail.com>
2017-12-30 22:22:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,6 +54,9 @@ class TicTacToe
     if valid_move?(index)
       move(index, current_player)
       display_board
+    else
+      inputs = gets.strip
+      index = input_to_index
     end
   end
 end
